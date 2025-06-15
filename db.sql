f72ad514e9691533c94a099e5f3b432e913611c5
AirtonRaphael <airton.raphael@hotmail.com>
2025-06-15 17:53:15 -0300

    Add db.sql file

---
+++ db.sql
@@ -0,0 +1,27 @@
+CREATE TABLE PermissionType (
+    permission_id INTEGER PRIMARY KEY,
+    permission_type TEXT NOT NULL UNIQUE
+);
+
+
+CREATE TABLE User (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL,
+    hash_password TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    permission_id INTEGER,
+    FOREIGN KEY (permission_id) REFERENCES PermissionType(permission_id)
+);
+
+
+INSERT INTO PermissionType (permission_type)
+VALUES ('User'), ('Admin');
+
+
+INSERT INTO User (username, hash_password, email, permission_id)
+VALUES (
+    'admin',
+    '$2b$12$nfdNjajPaqAZ83fqn3UsHe/7.aPy/6XG8H0v2J6un4H0iEZNRrECa',
+    'admin@admin.com',
+    (SELECT permission_id FROM PermissionType WHERE permission_type = 'Admin')
+);
