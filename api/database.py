721c82f013677ea82b9d6acc173f3cf4773b22b9
AirtonRaphael <airton@hotmail.com>
2025-06-23 20:51:56 -0300

    removed unnecessary print

--- api/database.py
+++ api/database.py
@@ -10,7 +10,6 @@ Base = declarative_base()
 
 def start_db() -> None:
     global SessionLocal
-    print(settings.DB_URL)
     engine = create_engine(settings.DB_URL, connect_args={"check_same_thread": False})
 
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
