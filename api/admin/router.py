c5c620eddc1f1eef6ee56077853432c22dfcc258
AirtonRaphael <airton@hotmail.com>
2025-06-23 22:36:04 -0300

    added user creation function and fixed access control in delete operation

--- api/admin/router.py
+++ api/admin/router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 
 from admin import services
-from auth.schema import UserResponse
-from auth.auth import admin_permission
+from auth.schema import UserResponse, CreateUserSchema
+from auth import auth
 
 router = APIRouter(
     prefix="/admin",
@@ -11,14 +11,21 @@ router = APIRouter(
 
 
 @router.get("/users", response_model=list[UserResponse])
-def list_users(user=Depends(admin_permission)):
+def list_users(req_user=Depends(auth.admin_permission)):
     users = services.get_users()
 
     return users
 
 
+@router.post("/create_user", response_model=UserResponse)
+def create_user(new_user: CreateUserSchema, req_user=Depends(auth.admin_permission)):
+    user = auth.create_user(new_user)
+
+    return user
+
+
 @router.delete("/delete_user")
-def delete_user(user_id: int):
+def delete_user(user_id: int, req_user=Depends(auth.admin_permission)):
     services.delete_user(user_id)
 
     return
